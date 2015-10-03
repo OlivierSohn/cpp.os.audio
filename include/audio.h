d21dccb87c657245762b40109c9859669602592f
Olivier Sohn <olivier.sohn@gmail.com>
2015-10-03 04:27:52 -0400

    audio frequency analysis using zero crossings

--- include/audio.h
+++ include/audio.h
@@ -1,5 +1,11 @@
 #pragma once
 #include <atomic>
+#include <algorithm>
+#include <numeric>
+#include "os.log.h"
+#include "math.h"
+#include "cg.math.numeric.h"
+#include "cg.math.filter.h"
 
 /* Select sample format. */
 #if 1
@@ -23,15 +29,315 @@ typedef char SAMPLE;
 #define PRINTF_S_FORMAT "%d"
 #endif
 
+#include <vector>
+
+const int SAMPLE_RATE(44100);
+
 namespace imajuscule
 {
+    enum class Result { NOT_ENOUGH_DATA, NOT_ENOUGH_MEMORY, OK, PREVIOUS_VALUE };
+
+    constexpr static const float minFreq = 80.f; // in herz (based on my voice :) )
+    constexpr static const float maxFreq = 440.f; // in herz (also based on my voice :) )
+    // and we want to have at least 3 samplings per period of max frequency, so:
+    constexpr static const float samples_per_max_freq_period = 8.f;
+    constexpr static const float time_between_representative_samples = 1.f / (samples_per_max_freq_period * maxFreq);
+    constexpr static const int sampling_period = SAMPLE_RATE * time_between_representative_samples;
+
+    struct FreqAlgo
+    {
+        virtual bool needsLock() = 0;
+        // if needsLock returns true, this is called in the main thread, while lock is taken
+        virtual void computeWhileLocked(){};
+        // called in the main thread, outside lock scope
+        virtual Result computeFrequency(float & f) = 0;
+
+    };
+    
+    enum CyclicInitializationType
+    {
+        OVERWRITE_INITIAL_VALUES_WITH_FIRST_FEED,
+        KEEP_INITIAL_VALUES
+    };
+    
+    template<class T, int KIND = KEEP_INITIAL_VALUES>
+    struct cyclic
+    {
+        using container = typename std::vector<T>;
+        using iterator = typename container::iterator;
+        using const_iterator = typename container::const_iterator;
+        
+        operator container & () { return buf; }
+        operator container const& () const { return buf; }
+        
+        const_iterator begin() const { return buf.begin();}
+        iterator begin() { return buf.begin();}
+        const_iterator cycleEnd() const { return it;}
+        iterator cycleEnd() { return it;}
+        const_iterator end() const { return buf.end();}
+        iterator end() { return buf.end();}
+
+        size_t size() const { return buf.size(); }
+        
+        cyclic(size_t size, T initVals)
+        :initialValue(initVals)
+        {
+            buf.resize(size, initVals);
+            it = buf.begin();
+        }
+        void feed(T val)
+        {
+            *it = val;
+            ++it;
+            if(it == buf.end())
+                it = buf.begin();
+            
+            if(isFirstFeed)
+            {
+                if(KIND == OVERWRITE_INITIAL_VALUES_WITH_FIRST_FEED)
+                {
+                    std::fill(buf.begin(), buf.end(), val);
+                }
+                isFirstFeed = false;
+            }
+        }
+        void reset()
+        {
+            std::fill(buf.begin(), buf.end(), initialValue);
+            it = buf.begin();
+            isFirstFeed = true;
+        }
+    private:
+        container buf;
+        iterator it;
+        T initialValue;
+        bool isFirstFeed = true;
+    };
+    
+    struct slidingAverage
+    {
+        slidingAverage(size_t size) :
+        values_(size, 0.f)
+        {
+        }
+        float feed(float val)
+        {
+            values_.feed(val);
+            return get();
+        }
+        float get() const
+        {
+            return std::accumulate(values_.begin(), values_.end(), 0.f) / (float) values_.size();
+        }
+        void reset()
+        {
+            values_.reset();
+        }
+    private:
+        cyclic<float, OVERWRITE_INITIAL_VALUES_WITH_FIRST_FEED> values_;
+    };
+    template <class T>
+    struct range
+    {
+        range(){min_ = 10; max_ = -10;} // to force assert if not set afterwards
+        range(T min, T max):
+        min_(min)
+        , max_(max)
+        {
+            A(min <= max);
+        }
+        T delta() const
+        {
+            T res = max_ - min_;
+            return res;
+        }
+        void set(T min, T max)
+        {
+            A(min <= max);
+            min_ = min;
+            max_ = max;
+        }
+        bool extend(T val)
+        {
+            if(val > max_)
+            {
+                max_ = val;
+                return true;
+            }
+            else if(val < min_)
+            {
+                min_ = val;
+                return true;
+            }
+            return false;
+        }
+        bool contains(T val) const
+        {A(max_>=min_);
+            return (val <= max_ && val >= min_);
+        }
+        T getMax() const {return max_;}
+        T getMin() const {return min_;}
+    private:
+        T min_, max_;
+    };
+    struct FreqFromZC : public FreqAlgo
+    {
+        // use constant 0.01 instead of 0 to not record zero crossing related to noise
+        const SAMPLE upperZero = 0.01f;
+        
+        bool needsLock () override {return true;}
+        void computeWhileLocked() override;
+        
+        void reset()
+        {
+            // dont reset previousFreq !
+
+            signal_range.set(0.f,0.f);
+            positive_zeros_dist.reset();
+            acc = 0;
+            bWasNeg = true;
+            counter = sampling_period;
+        }
+        
+        FreqFromZC()
+        : positive_zeros_dist(16, 0)
+        , signal_range(0.f,0.f)
+        {
+            filter_.initWithSampleRate(((float)SAMPLE_RATE)/(float)sampling_period, 50, false);
+        }
+        
+        void feed(SAMPLE val)
+        {
+            counter++;
+            if(counter < sampling_period)
+                return;
+            counter = 0;
+            
+            // high pass
+            filter_.feed(&val);
+            val = *filter_.filtered();
+            
+            signal_range.extend(val);
+            
+            if(val > upperZero && bWasNeg)
+            {
+                positive_zeros_dist.feed(acc);
+                acc = 0;
+                bWasNeg = false;
+            }
+            else
+            {
+                if(!bWasNeg && val< upperZero)
+                    bWasNeg = true;
+            }
+
+            acc++;
+        }
+        
+        Result computeFrequency(float & f) override;
+        
+    private:
+        int counter = sampling_period;
+        Filter<float, 1, FilterType::HIGH_PASS> filter_;
+        
+        cyclic<int> positive_zeros_dist; // zero crossing intervals are recorded over several time steps
+        int acc = 0;
+        bool bWasNeg = true;
+        
+        float previousFreq = 0.f;
+        range<float> signal_range; // range is representative of a single time step (except for very first calculation of a series)
+    };
+    
+    /*
+    // according to http://www.fon.hum.uva.nl/paul/papers/Proceedings_1993.pdf
+    struct FreqFromAutocorr : public FreqAlgo
+    {
+        bool needsLock () override {return true;}
+
+        static const size_t bufferLength = 2048; // TODO adapt wrt lowest freq, so that hanning window spans over 3 periods
+        static const size_t bufferWithPaddingLength = ceil_power_of_two(bufferLength + bufferLength/2);
+        FreqFromAutocorr()
+        {
+            fft1.resize(bufferWithPaddingLength);
+            withZeroPading.resize(bufferWithPaddingLength);
+            processing.reserve(bufferLength);
+            willProcess.reserve(bufferLength);
+            live.reserve(bufferLength);
+            hanning.reserve(bufferLength);
+            inv_hanning_autocorr.reserve(bufferLength);
+            for(int i=0; i<bufferLength; ++i)
+            {
+                float fi = ((float)i) / (float)bufferLength;
+                float coeff = 2.f*M_PI * fi ;
+                float c = cos(coeff);
+                
+                hanning.push_back( 0.5f - (0.5f * c) );
+                float s =sin(coeff);
+                
+                inv_hanning_autocorr.push_back( 1.f /
+                                               ( (1.f - fi)*(2.f/3.f + c/3.f) + s/(2.f*M_PI))
+                                               );
+            }
+            
+        }
+        ~FreqFromAutocorr();
+        
+        // called in the audio thread
+        void feed(SAMPLE val)
+        {
+            live.push_back(val);
+            if(live.size()==bufferLength)
+            {
+                willProcess.swap(live);
+                live.clear();
+            }
+        }
+        
+        // called in the main thread, while lock is taken
+        void computeWhileLocked() override;
+        // called in the main thread, outside lock scope
+        Result computeFrequency(float & f) override;
+    private:
+        // when live is full, it is swapped with willProcess.
+        // when computeFrequency is called, processing and willProcess are swapped
+        std::vector<SAMPLE> withZeroPading, processing, hanning, inv_hanning_autocorr, willProcess, live;
+        
+        typedef struct { // Ugly, to not have to include kissfft header here
+            SAMPLE r;
+            SAMPLE i;
+        }my_kiss_fft_cpx;
+        std::vector<my_kiss_fft_cpx> fft1;
+        
+        void * cfg = NULL;
+        void * cfg2 = NULL;
+        // in the main thread
+        float mean() const
+        {
+            SAMPLE mean(0.f);
+            for(auto i : processing)
+                mean += i;
+            return mean / (SAMPLE) bufferLength;
+        }
+    };
+    */
+    
     struct paTestData
     {
-        /*unsigned long lastWriteSampleCount = 0;
-        unsigned long maxFrameIndex = 0;
-        unsigned long numSamples = 0;
-        SAMPLE      *recordedSamples = NULL;*/
+        const size_t sizeSlidingAverage = 160;
+        
+        paTestData()
+        : avg(sizeSlidingAverage)
+        {}
+        
+        void step(const SAMPLE * inputBuffer, unsigned long framesPerBuffer);
+        
+        //FreqFromPeaks
+        //FreqFromAutocorr
+        FreqFromZC
+            algo_freq;
         SAMPLE maxAbsSinceLastRead = 0.f;
+        slidingAverage avg;
+
         std::atomic_bool used { false };
     };
     
@@ -41,18 +347,39 @@ namespace imajuscule
         static Audio & getInstance();
         void Init();
         void TearDown();
-        float get(float guiTime);
+        float getMaxAbs(float guiTime);
+        Result getFrequency(float guiTime, float & f);
         ~Audio();
     private:
-        Audio();
+        Audio() {};
         static Audio * gInstance;
         bool bInitialized_ = false;
         typedef void PaStream; //#include "portaudio.h"
         PaStream *stream = NULL;
         
         paTestData data;
-        bool computedOnce_ = false;
-        float timeComputation_;
-        SAMPLE value_;
+        
+        struct
+        {
+            bool hasResultForTime(float t) const
+            {
+                return (computedOnce_ && timeComputation_ == t);
+            }
+            float result() const
+            {
+                A(computedOnce_);
+                return value_;
+            }
+            void storeResultForTime(float res, float t)
+            {
+                computedOnce_ = true;
+                timeComputation_ = t;
+                value_ = res;
+            }
+        private:
+            bool computedOnce_ = false;
+            float timeComputation_;
+            float value_;
+        } maxAbs_result, freq_result;
     };
 }
--- source/audio.cpp
+++ source/audio.cpp
@@ -2,12 +2,17 @@
 #include <string.h>
 #include <cerrno>
 #include <algorithm>
+#include <functional>
 #include <cmath>
+#include <map>
 
 #include "audio.h"
 // SDL does not have audio recording yet so I use portaudio
 //#include "SDL_audio.h"
 #include "portaudio.h"
+// fft method is not easy to implement, i prefered zero crossing instead but i leave it here just in case
+//#include "kiss_fftr.h"
+#include <cstdlib>
 #include "os.log.h"
 using namespace imajuscule;
 
@@ -24,12 +29,11 @@ int setenv(const char *name, const char *value, int overwrite)
 }
 #endif
 
-const int SAMPLE_RATE(44100);
 const int FRAMES_PER_BUFFER(16);
 const float NUM_SECONDS(0.001f);
 const int NUM_CHANNELS(1);
-/* #define DITHER_FLAG     (paDitherOff)  */
-#define DITHER_FLAG     (0) /**/
+
+#define DITHER_FLAG     (0)
 
 #define CHECK_OVERFLOW  (0)
 #define CHECK_UNDERFLOW  (0)
@@ -47,72 +51,42 @@ static int recordCallback( const void *inputBuffer, void *outputBuffer,
                           void *userData )
 {
     paTestData *data = (paTestData*)userData;
-    const SAMPLE *rptr = (const SAMPLE*)inputBuffer;
-    /*
-    SAMPLE *wptr = &data->recordedSamples[0]; // or at data->frameIndex * NUM_CHANNELS to append after
-    data->lastWriteSampleCount = framesPerBuffer;
 
-    if(framesPerBuffer > data->maxFrameIndex)
-    {
-        data->lastWriteSampleCount = data->maxFrameIndex;
-        rptr += framesPerBuffer - data->maxFrameIndex; // to write the end of the buffer
-    }
-    long framesToCalc(data->lastWriteSampleCount);
-    */
-    //unsigned long framesLeft = data->maxFrameIndex - data->frameIndex;
-    
     (void) outputBuffer; /* Prevent unused variable warnings. */
     (void) timeInfo;
     (void) statusFlags;
     (void) userData;
     
-    static int c = 0;
-    
-    bool bFalse( false );
-    if (data->used.compare_exchange_strong(bFalse, true,
-                                           std::memory_order_acquire,
-                                           std::memory_order_relaxed))
-    {
-        c++;
-        // The lock succeeded
-        
-        /*if( inputBuffer == NULL )
-        {
-            for( long i=0; i<framesToCalc; i++ )
-            {
-                *wptr++ = SAMPLE_SILENCE;  // left
-                if( NUM_CHANNELS == 2 ) *wptr++ = SAMPLE_SILENCE;  // right
-            }
-        }
-        else
-        {
-            for( long i=0; i<framesToCalc; i++ )
-            {
-                *wptr++ = *rptr++;  // left
-                if( NUM_CHANNELS == 2 ) *wptr++ = *rptr++;  // right
-            }
-        }
-        */
-        if(inputBuffer)
-        {
-            for( long i=0; i<framesPerBuffer; i++ )
-                data->maxAbsSinceLastRead = std::max(data->maxAbsSinceLastRead, std::abs(*rptr++));
-        }
-        
-        data->used.store(false, std::memory_order_release);
-    }
-    else
-    {
-        LG(INFO,"failed after %d", c);
-        c=0;
-    }
-    
+    data->step((const SAMPLE*)inputBuffer, framesPerBuffer);
     
     return paContinue;
 }
+void paTestData::step(const SAMPLE *rptr, unsigned long framesPerBuffer)
+{
+    bool bFalse( false );
+    while (!used.compare_exchange_strong(bFalse, true,
+                                     std::memory_order_acquire,
+                                     std::memory_order_relaxed))
+    {}
+    
+    // The lock succeeded
+    if(rptr)
+    {
+        for( unsigned long i=0; i<framesPerBuffer; i++ )
+        {
+            auto val = *rptr++;
+            maxAbsSinceLastRead = std::max(maxAbsSinceLastRead, std::abs(val));
+            
+            // filter high frequencies
+            auto filtered_value = avg.feed(val);
+            algo_freq.feed(filtered_value);
+        }
+    }
+    
+    used.store(false, std::memory_order_release);
+}
 
 
-Audio::Audio() {}
 Audio::~Audio() {}
 Audio * Audio::gInstance = NULL;
 Audio& Audio::getInstance()
@@ -275,30 +249,392 @@ void Audio::TearDown()
         LG(ERR, "Audio::TearDown : was not initialized");
     }
 }
-float Audio::get(float guiTime)
+float Audio::getMaxAbs(float guiTime)
 {
     if(stream)
     {
-        if(!computedOnce_ || timeComputation_ != guiTime)
+        if(maxAbs_result.hasResultForTime(guiTime))
+            return maxAbs_result.result();
+        else
         {
-            computedOnce_ = true;
-            timeComputation_ = guiTime;
-
-            // NOTE we should make maxAbsSinceLastRead atomic instead and not use data.used at all, to be more efficient
-            
             // critical section
             while (data.used.exchange(true)) { }
             
-            value_ = data.maxAbsSinceLastRead;
+            // NOTE we could make maxAbsSinceLastRead atomic instead and not use data.used at all, to be more efficient
+            float value = data.maxAbsSinceLastRead;
             data.maxAbsSinceLastRead = 0.f;
             
-            data.used = false; // unlock
-        }
+            // unlock
+            data.used = false;
 
-        return value_;
+            maxAbs_result.storeResultForTime(value, guiTime);
+            
+            return value;
+        }
     }
     
     return 0.f;
 }
 
+Result Audio::getFrequency(float guiTime, float &value)
+{
+    Result res = Result::NOT_ENOUGH_DATA;
+    
+    if(stream)
+    {
+        if(freq_result.hasResultForTime(guiTime))
+        {
+            value = freq_result.result();
+            res = Result::OK;
+        }
+        else
+        {
+            if(data.algo_freq.needsLock())
+            {
+                // critical section
+                while (data.used.exchange(true)) { }
+                
+                data.algo_freq.computeWhileLocked();
+                
+                // unlock
+                data.used = false;
+            }
+            
+            res = data.algo_freq.computeFrequency(value);
+            A(res == Result::OK);
+            freq_result.storeResultForTime(value, guiTime);
+        }
+    }
+    
+    return res;
+}
 
+/*
+void FreqFromAutocorr::computeWhileLocked()
+{
+    processing.clear();
+    processing.swap(willProcess);
+}*/
+
+void plot(const char*title,const std::vector<SAMPLE>&v)
+{
+    return;
+    LG(INFO,"%s", title);
+    std::string s(v.size(), ' ');
+    float min_ = 0.f;
+    float max_ = 0.f;
+    
+    for(auto e:v)
+    {
+        min_ = std::min(min_, e);
+        max_ = std::max(max_, e);
+    }
+    
+    const int vSteps = 10;
+    
+    std::vector<std::string> vs(vSteps, s);
+    int i=0;
+    for(auto e:v)
+    {
+        auto normalized = (e - min_) / (max_ - min_);
+        size_t index = (size_t)(normalized * (float)vSteps);
+        if(index == vSteps)
+            index = vSteps-1;
+        vs[index][i] = '*';
+        i++;
+    }
+    
+    for(const auto & str:vs)
+    {
+        LG(INFO,"%s",str.c_str());
+    }
+    
+}
+/*
+auto FreqFromAutocorr::computeFrequency(float & f) -> Result
+{
+    if(processing.empty())
+        return Result::NOT_ENOUGH_DATA;
+    
+    /////////////////////////
+    // substract mean value
+    /////////////////////////
+    
+    {
+        auto mean_value = mean();
+        
+        for(auto & v:processing)
+        {
+            v -= mean_value;
+        }
+    }
+    
+    plot("mean substracted", processing);
+    
+    /////////////////////////
+    // multiply by hanning window
+    /////////////////////////
+    
+    std::transform(processing.begin(), processing.end(),
+                   hanning.begin(),
+                   processing.begin(),
+                   std::multiplies<SAMPLE>());
+    
+    plot("windowed", processing);
+
+    ////////////////////////
+    // pad with 0s
+    ////////////////////////
+    
+    {
+        size_t i = 0;
+        for(; i<bufferLength; i++)
+        {
+            withZeroPading[i] = processing[i];
+        }
+        for(;i<bufferWithPaddingLength; i++)
+        {
+            withZeroPading[i] = 0.f;
+        }
+    }
+    
+    plot("padded", withZeroPading);
+
+    ////////////////////////
+    // compute FFT
+    ////////////////////////
+    
+    {
+        if(!cfg)
+        {
+            cfg = kiss_fftr_alloc(bufferWithPaddingLength, 0, NULL, NULL);
+        }
+        
+        if(cfg)
+        {
+            kiss_fftr((kiss_fftr_cfg)cfg, withZeroPading.data(), (kiss_fft_cpx*)fft1.data());
+        }
+        else
+        {
+            LG(ERR, "not enough memory?");
+            return Result::NOT_ENOUGH_MEMORY;
+        }
+    }
+    
+    ////////////////////////
+    // square in frequenty domain
+    ////////////////////////
+    
+    {
+        // (a+bi)^2 = a^2 + 2abi - b^2
+        for(auto & c : fft1)
+        {
+            c.r = c.r * c.r + c.i * c.i;
+            c.i = 0;
+        }
+    }
+    
+    ////////////////////////
+    // compute FFT2
+    ////////////////////////
+    
+    {
+        if(!cfg2)
+        {
+            cfg2 = kiss_fftr_alloc(bufferWithPaddingLength, 1, NULL, NULL);
+        }
+        
+        if(cfg2)
+        {
+            kiss_fftri((kiss_fftr_cfg)cfg2, (const kiss_fft_cpx*)fft1.data(), withZeroPading.data());
+        }
+        else
+        {
+            LG(ERR, "not enough memory?");
+            return Result::NOT_ENOUGH_MEMORY;
+        }
+    }
+
+    plot("padded after ffts", withZeroPading);
+
+    /////////////////////////
+    // divide by hanning window autocorrelation
+    /////////////////////////
+    
+    std::transform(withZeroPading.begin(), withZeroPading.end(),
+                   inv_hanning_autocorr.begin(),
+                   withZeroPading.begin(),
+                   std::multiplies<SAMPLE>());
+    plot("compensation", withZeroPading);
+
+    f = 0.f;
+    SAMPLE peak = 0.f;
+    for(int i=bufferLength; i>bufferLength/2; --i)
+    {
+        int cmp = bufferLength-i;
+        withZeroPading[i] *= inv_hanning_autocorr[cmp];
+        if(withZeroPading[i]>peak)
+        {
+            f = (float )cmp;
+            peak = withZeroPading[i];
+        }
+    }
+    LG(INFO,"%.4f", f);
+    plot("divide by hauto", withZeroPading);
+    
+    // when divide by hanning autocorr, the last tiny part of the spectrum goes wild, maybe that was why there was an upsampling in the algorithm
+
+    return Result::OK;
+}
+
+FreqFromAutocorr::~FreqFromAutocorr()
+{
+    if( cfg )
+        free( cfg );
+    if( cfg2 )
+        free( cfg2 );
+}*/
+
+Result FreqFromZC::computeFrequency(float & f)
+{
+    f = previousFreq;
+    return Result::OK;
+}
+void FreqFromZC::computeWhileLocked()
+{
+    auto delta = signal_range.delta();
+    signal_range.set(0.f, 0.f);
+    
+    struct algo_freqFromZC
+    {
+        range<int> periodRange;
+        int total = 0;
+        int count = 0;
+        int cur=0;
+        bool hasStarted = false;
+        std::function<void(int)> step = [this](int interval){
+            if(!interval)
+                return;
+            
+            if(!hasStarted)
+            {
+                if(periodRange.contains(interval))
+                    hasStarted = true;
+                else
+                    return;
+            }
+            int next = cur + interval;
+            if(periodRange.contains(next))
+            {
+                total += next;
+                count ++;
+                cur = 0;
+            }
+            else if(periodRange.contains(next/2))
+            {
+                total += next;
+                count += 2;
+                cur = 0;
+            }
+            else if(next > periodRange.getMax() * 2)
+                cur = 0;
+            else
+                cur = next;
+        };
+    } data;
+    
+    // use ranges : 200 202 203 201 302 99 should give the 200 range
+
+    struct IntervalRange
+    {
+        IntervalRange(int initiator) :
+        initiator_(initiator)
+        , count_(1){
+            float jitter = ((float)initiator) * 0.15f;
+            int jitter_int = std::max(1, (int)(jitter+0.5f));
+            r_.set(initiator-jitter_int, initiator+jitter_int);
+        }
+        
+        bool tryInsert(int val)
+        {
+            if(r_.contains(val))
+            {
+                count_++;
+                return true;
+            }
+            return false;
+        }
+        int getCount() const { return count_;}
+        int getInitiator() const { return initiator_;}
+        range<int> const& getRange() const {return r_;}
+    private:
+        int initiator_;
+        range<int> r_;
+        int count_;
+    };
+    std::vector<IntervalRange> ranges;
+    
+    auto it = positive_zeros_dist.begin();
+    auto end = positive_zeros_dist.end();
+    for(;it!=end;++it)
+    {
+        auto interval = *it;
+        if(interval)
+        {
+            bool bFound = false;
+            for(auto & r:ranges)
+            {
+                if( r.tryInsert(interval))
+                {
+                    bFound = true;
+                    break;
+                }
+            }
+            
+            if(!bFound)
+            {
+                ranges.emplace_back(interval);
+            }
+        }
+    }
+
+    // ponderate frequency of occurences by initiator value so that 20 20 20 20 30 10 gives 20
+    //      and so that 4 4 4 4 8 8 gives 8
+    
+    int maxCount = 0;
+    for(auto const& r:ranges)
+    {
+        auto count = r.getCount() * r.getInitiator();
+        if(count > maxCount)
+        {
+            maxCount = count;
+            data.periodRange = r.getRange();
+        }
+    }
+
+    auto cycleStart = positive_zeros_dist.cycleEnd();
+    for_each(cycleStart, end, data.step);
+    for_each(positive_zeros_dist.begin(), cycleStart, data.step);
+
+    if(data.total)
+    {
+        // enable result if:
+        // all periods were reconstructed from zero-crossing intervals
+        // or the signal is loud enough to not be considered noise
+        if(data.count == positive_zeros_dist.size() || delta > 0.01f)
+        {
+            auto candidate = ((float)data.count) / ((float)data.total);
+            candidate *= ((float)SAMPLE_RATE)/(float)sampling_period;
+            if(candidate < minFreq )
+            {
+                //LG(INFO,"%.4f < %.4f : discarded", candidate, minFreq);
+            }
+            else if(candidate > maxFreq)
+            {
+                //LG(INFO,"%.4f > %.4f : discarded", candidate, maxFreq);
+            }
+            else
+                previousFreq = candidate;
+        }
+    }
+}
