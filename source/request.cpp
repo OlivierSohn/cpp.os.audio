ab1e3a736dab91f91470690f9c67edc8d41d6958
Olivier Sohn <olivier.sohn@gmail.com>
2016-12-24 12:55:54 -0500

    add files

---
+++ source/request.cpp
@@ -0,0 +1,22 @@
+
+using namespace imajuscule;
+
+Request::Request( Sounds & sounds, Sound const & sound, float freq_hz, float volume, float duration_ms ) :
+volume(volume)
+{
+    duration_in_samples = (int)( ((float)SAMPLE_RATE) * 0.001f * duration_ms );
+    
+    buffer = &sounds.get( { sound, freq_hz } );
+    A(buffer);
+    
+    if( sound.zeroOnPeriodBoundaries() ) {
+        const int period_size = (int)buffer->values.size();
+        
+        const int mod = duration_in_samples % period_size;
+        if(mod) {
+            duration_in_samples += period_size-mod;
+        }
+        A( 0 == duration_in_samples % period_size);
+    }
+}
+
