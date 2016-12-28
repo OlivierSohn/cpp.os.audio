37742026222789e20026553e87bb4e420dce4578
Olivier Sohn <olivier.sohn@gmail.com>
2016-12-27 22:29:35 -0500

    module public headers are included from core to outter

--- include/public.h
+++ include/public.h
@@ -3,7 +3,14 @@
 #include <atomic>
 #include <queue>
 #include <algorithm>
+#include <map>
 
+#include "../../algorithms/include/public.h"
+#include "../../tp.muparser/include/public.h"
+#include "../../os.log/include/public.h"
+#include "../../os.storage/include/public.h"
+#include "../../model/include/public.h"
+#include "../../wiremodeler/include/public.h"
 #include "../../cg.math/include/public.h"
 
 #include "sound.h"
