be7ef0e43d40da708dc16107f630817400027e09
Olivier Sohn <olivier.sohn@gmail.com>
2018-04-23 20:37:32 +0200

    Remove unused dependencies

--- include/public.h
+++ include/public.h
@@ -2,19 +2,18 @@
 
 #include <algorithm>
 #include <atomic>
-#include <complex>
 #include <map>
 #include <queue>
 #include <type_traits>
 #include <vector>
 
-#include "../../cpp.algorithms/include/public.h"
-#include "../../tp.muparser/include/public.h"
+#ifndef NO_AUDIO_IN
+# include "../../tp.muparser/include/public.h"
+# include "../../cg.math/include/public.h"
+#endif
+
 #include "../../cpp.os.logs/include/public.h"
-#include "../../cpp.os.storage/include/public.h"
 #include "../../cpp.model/include/public.h"
-#include "../../wiremodeler/include/public.h"
-#include "../../cg.math/include/public.h"
 #include "../../cpp.audio/include/public.h"
 
 #if TARGET_OS_IOS
