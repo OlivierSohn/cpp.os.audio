6740d83fa6a6a30bcc6a7f4bd0d99ff207d15654
Olivier Sohn <olivier.sohn@gmail.com>
2018-04-23 20:04:30 +0200

    Change paths

--- include/public.h
+++ include/public.h
@@ -8,14 +8,14 @@
 #include <type_traits>
 #include <vector>
 
-#include "../../algorithms/include/public.h"
+#include "../../cpp.algorithms/include/public.h"
 #include "../../tp.muparser/include/public.h"
-#include "../../os.log/include/public.h"
-#include "../../os.storage/include/public.h"
-#include "../../model/include/public.h"
+#include "../../cpp.os.logs/include/public.h"
+#include "../../cpp.os.storage/include/public.h"
+#include "../../cpp.model/include/public.h"
 #include "../../wiremodeler/include/public.h"
 #include "../../cg.math/include/public.h"
-#include "../../audio/include/public.h"
+#include "../../cpp.audio/include/public.h"
 
 #if TARGET_OS_IOS
 # import <AudioToolbox/AUComponent.h>
